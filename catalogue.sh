ad542ecd19a759b3c934c1dc58ffd696ec45ff5d
chandana24 <gantachandana98@gmail.com>
2023-08-12 23:31:54 +0530

    echo commands

--- catalogue.sh
+++ catalogue.sh
@@ -1,39 +1,39 @@
-echo -e "/e[32m<<<<<<<<<<Create catalogue service>>>>>>>>>/e[0m"
+echo -e "/e[36m<<<<<<<<<<Create catalogue service>>>>>>>>>/e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
 
-echo -e "/e[32m<<<<<<<<<<Create Mongo repo>>>>>>>>>>>>>>/e[0m"
+echo -e "/e[36m<<<<<<<<<<Create Mongo repo>>>>>>>>>>>>>>/e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo -e "/e[32m<<<<<<<<<Create nodejs repos>>>>>>>>/e[0m"
+echo -e "/e[36m<<<<<<<<<Create nodejs repos>>>>>>>>/e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo -e "/e[32m<<<<<<<<<Install nodejs>>>>>>>>>>>>/e[0m"
+echo -e "/e[36m<<<<<<<<<Install nodejs>>>>>>>>>>>>/e[0m"
 yum install nodejs -y
 
-echo -e  "/e[32m<<<<<<<<Create user application>>>>>>>/e[0m"
+echo -e  "/e[36m<<<<<<<<Create user application>>>>>>>/e[0m"
 useradd roboshop
 
-echo -e  "/e[32m<<<<<<Create application directory>>>>>>>/e[0m"
+echo -e  "/e[36m<<<<<<Create application directory>>>>>>>/e[0m"
 mkdir /app
 
-echo -e "/e[32m<<<<<<<<Download application content>>>>>>>>>/e[0m"
+echo -e "/e[36m<<<<<<<<Download application content>>>>>>>>>/e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 
-echo -e "/e[32m<<<<<<<<<<<Extract application content>>>>>>>>>>>>/e[0m"
+echo -e "/e[36m<<<<<<<<<<<Extract application content>>>>>>>>>>>>/e[0m"
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
 
-echo -e "/e[32m<<<<<<<<<<Download nodejs dependencies>>>>>/e[0m"
+echo -e "/e[36m<<<<<<<<<<Download nodejs dependencies>>>>>/e[0m"
 npm install
 
-echo -e "/e[32m<<<<<<<<Install mongodb client>>>>>>>>>>/e[0m"
+echo -e "/e[36m<<<<<<<<Install mongodb client>>>>>>>>>>/e[0m"
 yum install mongodb-org-shell -y
 
-echo -e "/e[32m<<<<<<Load catalogue schemaa>>>>>>/e[0m"
+echo -e "/e[36m<<<<<<Load catalogue schemaa>>>>>>/e[0m"
 mongo --host mongodb.chandana24.online </app/schema/catalogue.js
 
-echo -e "/e[32m<<<<<<<Start Catalogue service>>>>>>/e[0m"
+echo -e "/e[36m<<<<<<<Start Catalogue service>>>>>>/e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
