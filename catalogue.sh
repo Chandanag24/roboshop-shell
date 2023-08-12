15456ab497cd383e62ad5c519542ea274d5ded62
chandana24 <gantachandana98@gmail.com>
2023-08-12 22:55:02 +0530

    Roboshop scripts updates DNS RECORDS

--- catalogue.sh
+++ catalogue.sh
@@ -1,18 +1,39 @@
+echo "<<<<<<<<<<Create catalogue service>>>>>>>>>>"
 cp catalogue.service /etc/systemd/system/catalogue.service
+
+echo "<<<<<<<<<<Create Mongo repo>>>>>>>>>>>>>>>"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo "<<<<<<<<<Create nodejs repos>>>>>>>>>"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo "<<<<<<<<<Install nodejs>>>>>>>>>>>>>"
 yum install nodejs -y
+
+echo "<<<<<<<<Create user application>>>>>>>>"
 useradd roboshop
+
+echo "<<<<<<Create application directory>>>>>>>>"
 mkdir /app
+
+echo "<<<<<<<<Download application content>>>>>>>>>>"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
+echo "<<<<<<<<<<<extract application content>>>>>>>>>>>>>"
 cd /app
 unzip /tmp/catalogue.zip
-#cd /app
-#npm install
+cd /app
 
-#yum install mongodb-org-shell -y
-#mongo --host mongodb.chandana24.online </app/schema/catalogue.js
+echo "<<<<<<<<<<Download nodejs dependencies>>>>>>"
+npm install
 
-#systemctl daemon-reload
-#systemctl enable catalogue
-#systemctl restart catalogue
+echo "<<<<<<<<Install mongodb client>>>>>>>>>>>"
+yum install mongodb-org-shell -y
+
+echo "<<<<<<<Load catalogue schemaa>>>>>>>"
+mongo --host mongodb.chandana24.online </app/schema/catalogue.js
+
+echo "<<<<<<<Start Catalogue service>>>>>>>"
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl restart catalogue
