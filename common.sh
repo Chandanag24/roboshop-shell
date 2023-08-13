c5f702d2f462748f986a15b0aae0a674e1969844
chandana24 <gantachandana98@gmail.com>
2023-08-13 15:57:22 +0530

    Functions in Roboshop

--- common.sh
+++ common.sh
@@ -1,33 +1,30 @@
 log=/tmp/roboshop.log
+
 func_apppre() {
-    log=/tmp/roboshop.log
-  echo -e "\e[35m<<<<<<<<<<Create Application User>>>>>>>>>\e[0m"
-    useradd roboshop &>>log
-
-    echo -e "\e[36m<<<<<<<<<<Create App Dir>>>>>>>>>\e[0m"
-    mkdir /app &>>log
-
-    echo -e "\e[36m<<<<<<<<<<Download App Content>>>>>>>>>\e[0m"
-    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip  &>>log
-
-    echo -e "\e[34m<<<<<<<<<<Extract App Content>>>>>>>>>\e[0m"
-    cd /app
-    unzip /tmp/${component}.zip &>>log
-    cd /app
-}
-func_systemd() {
-   log=/tmp/roboshop.log
-  echo -e "\e[36m<<<<<<<<<<Start ${component} Service>>>>>>>>>\e[0m"
-    systemctl daemon-reload &>>log
-    systemctl enable ${component} &>>log
-    systemctl restart ${component} &>>log
-}
-func_nodejs() {
-  log=/tmp/roboshop.log
-
   echo -e "\e[35m<<<<<<<<<<Create User Service>>>>>>>>>\e[0m"
   cp ${component}.service /etc/systemd/system/${component}.service &>>log
 
+  echo -e "\e[35m<<<<<<<<<<Create Application User>>>>>>>>>\e[0m"
+  useradd roboshop &>>log
+
+  echo -e "\e[36m<<<<<<<<<<Create App Dir>>>>>>>>>\e[0m"
+  mkdir /app &>>log
+
+  echo -e "\e[36m<<<<<<<<<<Download App Content>>>>>>>>>\e[0m"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip  &>>log
+
+  echo -e "\e[34m<<<<<<<<<<Extract App Content>>>>>>>>>\e[0m"
+  cd /app
+  unzip /tmp/${component}.zip &>>log
+  cd /app
+}
+func_systemd() {
+  echo -e "\e[36m<<<<<<<<<<Start ${component} Service>>>>>>>>>\e[0m"
+  systemctl daemon-reload &>>log
+  systemctl enable ${component} &>>log
+  systemctl restart ${component} &>>log
+}
+func_nodejs() {
   echo -e "\e[35m<<<<<<<<<<Create Mongo Repo>>>>>>>>>\e[0m"
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>log
 
@@ -52,10 +49,6 @@ func_nodejs() {
 }
 
 func_java() {
-
-  echo -e "\e[36m<<<<<<<<<<Create ${component} Service>>>>>>>>>\e[0m"
-  cp ${component}.service /etc/systemd/system/${component}.service &>>log
-
   echo -e "\e[36m<<<<<<<<<<Install Maven>>>>>>>>>\e[0m"
   yum install maven -y &>>log
 
@@ -72,6 +65,17 @@ func_java() {
 
   echo -e "\e[36m<<<<<<<<Load Schema >>>>>>>>>\e[0m"
   mysql -h mysql.chandana24.online -uroot -pRoboShop@1 < /app/schema/ ${component}.sql &>>log
+
+  func_systemd
+}
+func_python() {
+  echo -e "\e[36m<<<<<<<<Install Python >>>>>>>>>\e[0m"
+  yum install python36 gcc python3-devel -y  &>>log
+
+  func_apppre
+
+  echo -e "\e[36m<<<<<<<<Bulid ${component} Service>>>>>>>>>\e[0m"
+  pip3.6 install -r requirements.txt  &>>log
 
   func_systemd
 }
